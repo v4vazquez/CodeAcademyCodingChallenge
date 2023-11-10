20bb26748a619fe33a07040d59fa728e17557b8b
Eric Vazquez <ericvazquez7992@gmail.com>
2023-11-09 19:10:38 -0500

    pushed the other questions cause why not

---
+++ src/main/java/org/example/ArraySort.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class ArraySort {
+    You could create a function for this solution as well, but be sure to try your program with varying lengths and types
+    of arrays. Try one with all integers, another with negative numbers, and another with decimals.
+
+}
---
+++ src/main/java/org/example/DivisibleBy10.java
@@ -0,0 +1,6 @@
+package org.example;
+Here, you’ll create a function that’ll give you a “true” or “false” Boolean as its output.
+        The inputted number should only return a “true” if it’s divisible by 10.
+        Otherwise, your program should return a “false” answer.
+public class DivisibleBy10 {
+}
